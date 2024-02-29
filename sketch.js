2e93947087ac88121dff740caf9ce58680682d89
unknown <tfig2003@gmail.com>
2024-02-29 14:49:57 +0000

    desenho maior

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 function setup() {
-    createCanvas(1920, 1080);
+    createCanvas(Windowwidth, windowheight);
     background(0,250,154);
   }
   
   function draw() {
-    rect(mouseX, mouseY, 50, 35);
+    rect(mouseX, mouseY, 150, 105);
   }
